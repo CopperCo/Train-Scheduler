f8bc1230837b131db5c34c14583fc4a66d0049b7
Hayley K <mrs.kornilenko@gmail.com>
2018-07-25 15:15:10 -0400

    added styling to index.html//added time fuctions for the calculations

--- assets/javascript/trainapp.js
+++ assets/javascript/trainapp.js
@@ -1,84 +1,98 @@
-moment().format("MMMM Do YYYY, h:mm:ss a");
+// Shows current time (Military Style 24)
+$('#currentTime').append(moment().format('HH:mm'));
 
-function nextTrainArrival() {
-  var currentTime = moment().format("hh:mm:ss A");
-  console.log(currentTime);
-}
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyDLC7kZXVlB5DhxPeuUkJzMeTScGsXGyHQ',
+  authDomain: 'trainschedule-9cd76.firebaseapp.com',
+  databaseURL: 'https://trainschedule-9cd76.firebaseio.com',
+  projectId: 'trainschedule-9cd76',
+  storageBucket: '',
+  messagingSenderId: '795808002443'
+};
 
-$(document).ready(function() {
-  $("#currentTime").append(moment().format("hh:mm:ss A"));
-  console.log("ready!");
+firebase.initializeApp(config);
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDLC7kZXVlB5DhxPeuUkJzMeTScGsXGyHQ",
-    authDomain: "trainschedule-9cd76.firebaseapp.com",
-    databaseURL: "https://trainschedule-9cd76.firebaseio.com",
-    projectId: "trainschedule-9cd76",
-    storageBucket: "",
-    messagingSenderId: "795808002443"
-  };
+var database = firebase.database();
 
-  firebase.initializeApp(config);
+// Initial Values
+var trainName = '';
+var destination = '';
+var firstTrainTime = '';
+var frequency = '';
 
-  var database = firebase.database();
+// Capture Form Values
+$('#trainSubmission').on('click', function(event) {
+  event.preventDefault();
 
-  // Initial Values
-  var trainName = "";
-  var destination = "";
-  var firstTrainTime = "";
-  var frequency = "";
-  // Calculated Values
-  var nextArrival = "";
-  var minutesAway = 0;
-
-  // Capture Form Values
-  $("#trainSubmission").on("click", function(event) {
-    event.preventDefault();
-
-    trainName = $("#trainName-input")
+  trainName = $('#trainName-input')
+    .val()
+    .trim();
+  destination = $('#destination-input')
+    .val()
+    .trim();
+  firstTrainTime = moment(
+    $('#firstTrainTime-input')
       .val()
-      .trim();
-    destination = $("#destination-input")
-      .val()
-      .trim();
-    firstTrainTime = $("#firstTrainTime-input")
-      .val()
-      .trim();
-    frequency = $("#frequency-input")
-      .val()
-      .trim();
+      .trim(),
+    'HH:mm'
+  ).format('X');
+  frequency = $('#frequency-input')
+    .val()
+    .trim();
 
-    // Code for the push
-    database.ref().push({
-      trainName: trainName,
-      destination: destination,
-      firstTrainTime: firstTrainTime,
-      frequency: frequency,
-      dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+  // Code for the push
+  database.ref().push({
+    trainName: trainName,
+    destination: destination,
+    firstTrainTime: firstTrainTime,
+    frequency: frequency,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
   });
 
-  // Firebase watcher + table row loader
-  database.ref().on(
-    "child_added",
-    function(childSnapshot) {
-      $(".trainTable").append(
-        "<tr><td class='trainName'> " +
-          childSnapshot.val().trainName +
-          " </td><td class='destination'> " +
-          childSnapshot.val().destination +
-          " </td><td class='frequency'> " +
-          childSnapshot.val().frequency +
-          " </td><td class='firstTrainTime'> " +
-          childSnapshot.val().firstTrainTime +
-          " </td></tr>"
-      );
+  // Clears all of the text-boxes
+  $('#trainName-input').val('');
+  $('#destination-input').val('');
+  $('#firstTrainTime-input').val('');
+  $('#frequency-input').val('');
 
-      // Handle the errors
-    },
-    function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    }
-  );
+  return false;
 });
+
+// Firebase watcher + table row loader
+database.ref().on(
+  'child_added',
+  function(childSnapshot) {
+    var trainNames = childSnapshot.val().trainName;
+    var trainDest = childSnapshot.val().destination;
+    var trainFrequency = childSnapshot.val().frequency;
+    var theFirstTrain = childSnapshot.val().firstTrainTime;
+
+    var timeRemainder =
+      moment().diff(moment.unix(theFirstTrain), 'minutes') % trainFrequency;
+    var timeInMin = trainFrequency - timeRemainder;
+
+    var tArrival = moment()
+      .add(timeInMin, 'm')
+      .format('HH:mm');
+
+    $('.trainTable').append(
+      "<tr><td class='trainName'> " +
+        trainNames +
+        " </td><td class='destination'> " +
+        trainDest +
+        " </td><td class='frequency'> " +
+        trainFrequency +
+        " </td><td class='nextTrainArrival'> " +
+        tArrival +
+        "</td><td class='minutesToArrive'> " +
+        timeInMin +
+        '</td></tr>'
+    );
+
+    // Handle the errors
+  },
+  function(errorObject) {
+    console.log('Errors handled: ' + errorObject.code);
+  }
+);
